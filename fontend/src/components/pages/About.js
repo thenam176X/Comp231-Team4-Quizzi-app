4b4579ef221abf5e3fa7ed77c29df7ae7fe86440
unknown <nguyenquethi186@gmail.com>
2023-07-23 04:44:25 -0400

    update about and contact page

--- fontend/src/components/pages/About.js
+++ fontend/src/components/pages/About.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import '../../App.css';
-import Footer from '../assets/js/Footer';
-export default function About() {
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+const About = () => {
   return (
-    <>
-      <h1 className='about'>About Us</h1>;
-      <Footer />
-    </>
-  ); 
-}
+    <Container>
+      <h1>About Us</h1>
+      <p>Some text about who we are and what we do.</p>
+      <h2>Our Team</h2>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Jane Doe</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">CEO & Founder</Card.Subtitle>
+              <Card.Text>Some text that describes Jane.</Card.Text>
+              <Card.Link href="mailto:jane@example.com">jane@example.com</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Mike Ross</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Art Director</Card.Subtitle>
+              <Card.Text>Some text that describes Mike.</Card.Text>
+              <Card.Link href="mailto:mike@example.com">mike@example.com</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default About;
--- fontend/src/components/pages/Contact.js
+++ fontend/src/components/pages/Contact.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import '../../App.css';
-import Footer from '../assets/js/Footer';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-export default function Contact() {
+const ContactPage = () => {
   return (
-    <>
-      <h1 className='contact'>Contact Us</h1>;
-      <Footer />
-    </>
-  ); 
-}
+    <Container>
+      <Row>
+        <Col>
+          <h2>Contact Information</h2>
+          <Form>
+            <Form.Group controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter your email" />
+            </Form.Group>
+            <Form.Group controlId="formMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+        <Col>
+          <h2>Company Information</h2>
+          <p>Address: 123 Main Street, Anytown USA</p>
+          <p>Phone: (123) 456-7890</p>
+          <p>Email: info@company.com</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ContactPage;

9cd05416c7d2e51f51438fe99db8f84c2c5abb06
thenam176Xz <113480536+thenam176Xz@users.noreply.github.com>
2023-08-13 21:00:26 -0400

    Update GradeQuiz.js

--- frontend/src/components/pages/GradeQuiz.js
+++ frontend/src/components/pages/GradeQuiz.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
 const QuizContainer = styled.div`
@@ -11,14 +11,7 @@ const QuizContainer = styled.div`
 
 const QuizTitle = styled.h2`
   color: #333;
-  font-size: 1.5em;
-`;
-
-const QuestionContainer = styled.div`
-  margin-top: 1em;
-  padding: 1em;
-  border: 1px solid #ddd;
-  border-radius: 5px;
+  font-size: 24px;
 `;
 
 const QuestionTitle = styled.h3`
@@ -32,81 +25,52 @@ const AnswerTitle = styled.h3`
 const AnswerText = styled.p`
   color: ${props => props.isCorrect ? 'green' : 'black'};
 `;
+
 const ScoreContainer = styled.div`
   margin-top: 1em;
   font-size: 1.5em;
-  font-weight: bold;
+`;
+const QuestionText = styled.p`
+white-space: pre-line;
+`;
+const AnswerText1 = styled.p`
+color: ${props => props.isCorrect ? 'green' : 'red'};
+text-align: center;
+`;
+const QuestionContainer = styled.div`
+  margin-top: 1em;
+  padding: 1em;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 `;
 const GradeQuizPage = () => {
-  const [quizzes, setQuizzes] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/user/quiz/latest')
-      .then((response) => {
-        setQuizzes([response.data]); // Wrap the single quiz in an array
-      })
-      .catch((error) => {
-        console.error('Error fetching latest quiz:', error);
-      });
-  }, []);
-  const [quizResult, setQuizResult] = useState(null);
-  const [score, setScore] = useState(0);
-
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/user/quiz/result/latest')
-      .then((response) => {
-        setQuizResult(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching quiz result:', error);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (quizResult) {
-      let correctAnswers = 0;
-      quizResult.userAnswers.forEach((answer, index) => {
-        if (answer.userAnswer === answer.correctAnswerIndex) {
-          correctAnswers++;
-        }
-      });
-      setScore(correctAnswers);
-    }
-  }, [quizResult]);
-
-  if (!quizResult) {
-    return <div>Loading...</div>;
-  }
-  const QuestionText = styled.p`
-    white-space: pre-line;
-  `;
+  const history = useHistory();
+  const takenQuizData = history.location.state.takenQuizData;
 
   return (
     <QuizContainer>
-     <h1>Quiz Result</h1>
-      <ScoreContainer>You scored {score} out of {quizResult.userAnswers.length}</ScoreContainer> {/* Display the score */}
-      
-      {quizzes.map((quiz, index) => (
-        <div key={index}>
-          <QuizTitle>{quiz.title}</QuizTitle>
-          {quiz.questions.map((question, qIndex) => (
-            <QuestionContainer key={qIndex}>
-              <QuestionTitle>Question {qIndex + 1}</QuestionTitle>
-              <QuestionText dangerouslySetInnerHTML={{ __html: question.question }} />
-              <h4>Time Limit: {question.timeLimit} seconds</h4> {/* This line displays the time limit */}
-              <AnswerTitle>Answers:</AnswerTitle>
-              {question.answers.map((answer, aIndex) => (
-                <AnswerText key={aIndex} isCorrect={aIndex === question.correctAnswerIndex}>
-                  {answer}
-                </AnswerText>
-                
-              ))}
-               <AnswerTitle>Student answer:</AnswerTitle>
-    <AnswerText>{quizResult.userAnswers[qIndex].userAnswer}</AnswerText>
-            </QuestionContainer>
-          ))}
-        </div>
+       <h1>Quiz Results</h1>
+      <QuizTitle>{takenQuizData.title}</QuizTitle>
+      <ScoreContainer>Score: {takenQuizData.score}</ScoreContainer>
+      {takenQuizData.userAnswers.map((answer, index) => (
+        <QuestionContainer  key={index}>
+          <QuestionTitle>Question {index + 1}</QuestionTitle>
+          <QuestionText dangerouslySetInnerHTML={{ __html: answer.question }} />
+          <h4>Time Limit: {answer.timeLimit} seconds</h4>
+          <AnswerTitle>Answers: </AnswerTitle>          
+            {answer.answer.map((ans, aIndex) => (
+               <AnswerText key={aIndex} isCorrect={ans === answer.correctAnswer}>
+                   {ans}
+               </AnswerText>
+            ))}
+          <AnswerTitle>Your Answer:  </AnswerTitle>
+          <AnswerText>{answer.userAnswer}</AnswerText>
+          <AnswerText1 isCorrect={answer.userAnswer === answer.correctAnswer}>
+            {answer.userAnswer === answer.correctAnswer ? 'Correct' : 'Incorrect'}
+          </AnswerText1>
+        </QuestionContainer>
       ))}
+      
     </QuizContainer>
   );
 };
